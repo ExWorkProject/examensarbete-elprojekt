8946589aaf5991f400ec36ba2784b5c9c7cdb4ef
fredrik <freedrik92@hotmail.com>
2026-05-06 11:10:31 +0200

    updated watch for development, remove unused form in kontakt

--- EL_Arne/obj/Debug/net10.0/EL_Arne.AssemblyInfo.cs
+++ EL_Arne/obj/Debug/net10.0/EL_Arne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EL_Arne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c317519d3a8a5357c1e82bcd00a2018d4206a8bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EL_Arne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EL_Arne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
